a10341b3a991265ca87ae1ab168f6b8253904765
Hector Matias Reartes <hmatiasreartes5@hotmail.com>
2020-02-12 12:20:09 -0300

    Agregando la ruta de Tareas

--- servidor/index.js
+++ servidor/index.js
@@ -17,6 +17,7 @@ app.use(express.json({extended:true}))
 app.use('/api/usuarios',require('./routes/usuarios'));
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/proyectos',require('./routes/proyectos'));
+app.use('/api/tareas',require('./routes/tareas'));
 
 //arrancar la app
 app.listen(PORT, ()=> {
